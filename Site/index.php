f5db212d7fcceaea437ead71dbcb61abb3b059a4
Jan <Jan.blatter@cpnv.ch>
2020-06-09 18:55:07 +0200

    commit

--- Site/index.php
+++ Site/index.php
@@ -25,7 +25,7 @@ if (isset($_GET['action'])){                        /* Si action est activé , a
         case'loginIsCorrect':
             LoginIsCorrect($_POST);
             break;
-        case'RegisterIsCorrect':
+        case'registerIsCorrect':
             registerIsCorrect($_POST);
             break;
         default:
